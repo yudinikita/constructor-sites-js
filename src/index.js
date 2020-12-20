c1763bc03b63f500d0254a5ece9533afbfd31ffe
nBlackNinja <nikitayudin782@gmail.com>
2020-12-20 18:13:11 +0300

    create gitignore and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const model = [
-	
-]
+  { type: 'title', value: 'Hello, World!' },
+  { type: 'text', value: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit' },
+  { type: 'columns', value: ['111', '222', '333'] },
+];
