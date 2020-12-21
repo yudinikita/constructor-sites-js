921f491c0a0e040407ab8d36cc796b1d9694792f
nBlackNinja <nikitayudin782@gmail.com>
2020-12-21 18:32:14 +0300

    refactoring object with class

---
+++ src/classes/blocks.js
@@ -0,0 +1,7 @@
+export class Block {
+	constructor(type, value, options) {
+		this.type = type;
+		this.options = options;
+		this.value = value;
+	}
+}
--- src/model.js
+++ src/model.js
@@ -1,23 +1,27 @@
 import image from './assets/image.jpg';
+import {Block} from './classes/blocks';
 
 export const model = [
-  { type: 'title', value: 'Конструктор сайт на чистом JavaScript', options: {
+  new Block('title', 'Конструктор сайт на чистом JavaScript', {
     tag: 'h2',
     styles: {
       color: 'orange',
       padding: '1.5rem',
       'text-align': 'center'
-    }}},
-  { type: 'text', value: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit', options: {
+    }
+  }),
+  new Block('text', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit', {
     styles: {
       'font-size': '18px',
-    }}},
-  { type: 'columns', value: ['111', '222', '333', '444'], options: {
+    }
+  }),
+  new Block('columns', ['111', '222', '333', '444'], {
     styles: {
       background: '#f9f9f9',
       padding: '2rem',
-    }}},
-  { type: 'image', value: image, options: {
+    }
+  }),
+  new Block('image', image, {
     styles: {
       padding: '2rem 0',
       display: 'flex',
@@ -30,5 +34,5 @@ export const model = [
       height: 'auto'
     },
     alt: 'Картинка'
-  }}
+  })
 ];
